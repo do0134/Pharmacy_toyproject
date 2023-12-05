ace6940b4b8a9b4ca29dcbd555748db5a777a22a
do0134 <do0134@naver.com>
2023-12-05 16:31:17 +0900

    init commit

---
+++ find_pharmacy/src/main/java/com/example/find_pharmacy/FindPharmacyApplication.java
@@ -0,0 +1,14 @@
+package com.example.find_pharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class FindPharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindPharmacyApplication.class, args);
+	}
+
+}
---
+++ find_pharmacy/src/test/java/com/example/find_pharmacy/FindPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.find_pharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindPharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
