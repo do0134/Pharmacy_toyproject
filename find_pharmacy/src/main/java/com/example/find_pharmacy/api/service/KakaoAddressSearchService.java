af4baa52dab118ae3e54eb2b1c37c9e2e0544ff7
do0134 <do0134@naver.com>
2023-12-07 23:18:00 +0900

    feat: setting dto for kakaoapi

---
+++ find_pharmacy/src/main/java/com/example/find_pharmacy/api/service/KakaoAddressSearchService.java
@@ -0,0 +1,40 @@
+package com.example.find_pharmacy.api.service;
+
+import com.example.find_pharmacy.api.dto.KakaoApiResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KakaoAddressSearchService {
+
+    private final RestTemplate restTemplate;
+    private final KakaoUriBuilderService kakaoUriBuilderService;
+
+    @Value("${kakao.rest.api.key}")
+    private String kakaoRestApiKey;
+
+    public KakaoApiResponseDto requestAddressSearch(String address) {
+        if (ObjectUtils.isEmpty(address)) {
+            return null;
+        }
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(HttpHeaders.AUTHORIZATION, kakaoRestApiKey);
+        HttpEntity httpEntity = new HttpEntity<>(httpHeaders);
+
+        URI uri = kakaoUriBuilderService.buildUriByAddressSearch(address);
+        return restTemplate.exchange(uri, HttpMethod.GET, httpEntity, KakaoApiResponseDto.class).getBody();
+    }
+
+}
